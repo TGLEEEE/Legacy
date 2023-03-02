95022f990519524c4e1403c47182615c230de777
yoponchik <darkavengerpark@gmail.com>
2023-03-02 09:32:51 +0900

    Update Legacy.Build.cs

--- Source/Legacy/Legacy.Build.cs
+++ Source/Legacy/Legacy.Build.cs
@@ -8,7 +8,7 @@ public class Legacy : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "EnhancedInput" , "HeadMountedDisplay"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
