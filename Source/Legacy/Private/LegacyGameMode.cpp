f4f39f395fa4841ea87f5e72bc1b581031dc5381
TGLEEEE <121008880+TGLEEEE@users.noreply.github.com>
2023-03-08 19:09:54 +0900

    feat: timerwidget 구현

--- Source/Legacy/Private/LegacyGameMode.cpp
+++ Source/Legacy/Private/LegacyGameMode.cpp
@@ -2,4 +2,29 @@
 
 
 #include "LegacyGameMode.h"
+#include "Blueprint/UserWidget.h"
 
+void ALegacyGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	WorldTimer();
+	timerWidgetUI = CreateWidget(GetWorld(), timerWidgetFactory);
+	timerWidgetUI->AddToViewport();
+}
+
+void ALegacyGameMode::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+}
+
+void ALegacyGameMode::WorldTimer()
+{
+	GetWorldTimerManager().SetTimer(worldtimerHandle, FTimerDelegate::CreateLambda([&]()
+		{
+			worldTime++;
+			worldTimeSec = worldTime % 60;
+			worldTimeMin = worldTime / 60;
+			UE_LOG(LogTemp, Error, TEXT("world time is : %d"), worldTime);
+		}), 1.f, true);
+}
