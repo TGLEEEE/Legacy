4e447e1ed9d4257c4fba5f21874cc97ec374f163
yoponchik <darkavengerpark@gmail.com>
2023-03-20 19:08:12 +0900

    Update TimerWidget.cpp

--- Source/Legacy/Private/TimerWidget.cpp
+++ Source/Legacy/Private/TimerWidget.cpp
@@ -22,19 +22,19 @@ void UTimerWidget::NativeConstruct()
 	player = Cast<ALegacyPlayer>(GetWorld()->GetFirstPlayerController()->GetCharacter());
 }
 
-void UTimerWidget::NativeConstruct()
-{
-	Super::NativeConstruct();
-	// 게임모드 캐스팅
-	gm = Cast<ALegacyGameMode>(GetWorld()->GetAuthGameMode());
-	// 0.2초마다 업데이트 되게 타이머 세팅
-	GetWorld()->GetTimerManager().SetTimer(hdForTimerWidget, FTimerDelegate::CreateLambda([&]()
-		{
-			UpdateTimerWidget();
-		}), 0.2f, true);
-	// 플레이어 체력 읽어오기 위해 캐스팅
-	player = Cast<ALegacyPlayer>(GetWorld()->GetFirstPlayerController()->GetCharacter());
-}
+//void UTimerWidget::NativeConstruct()
+//{
+//	Super::NativeConstruct();
+//	// 게임모드 캐스팅
+//	gm = Cast<ALegacyGameMode>(GetWorld()->GetAuthGameMode());
+//	// 0.2초마다 업데이트 되게 타이머 세팅
+//	GetWorld()->GetTimerManager().SetTimer(hdForTimerWidget, FTimerDelegate::CreateLambda([&]()
+//		{
+//			UpdateTimerWidget();
+//		}), 0.2f, true);
+//	// 플레이어 체력 읽어오기 위해 캐스팅
+//	player = Cast<ALegacyPlayer>(GetWorld()->GetFirstPlayerController()->GetCharacter());
+//}
 
 void UTimerWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 {
